80a2b9731769833f08b575322f7cb8ce761498c9
itsleejames <176151266+itsleejames@users.noreply.github.com>
2025-11-19 15:11:23 +0000

    basic homepage

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import { Routes, Route } from "react-router-dom";
-import SearchPage from "./pages/SearchPage";
 import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import NavBar from "./components/layout/NavBar";
+import Footer from "./components/layout/Footer";
 
 export default function App() {
     return (
         <>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/search" element={<SearchPage />} />
-            </Routes>
+            <NavBar />
+            <div className="container mt-4">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/search" element={<SearchPage />} />
+                </Routes>
+            </div>
+            <Footer />
         </>
     );
 }
