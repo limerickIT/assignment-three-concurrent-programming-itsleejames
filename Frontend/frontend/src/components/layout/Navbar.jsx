ea83c5bb90271402cf2e44dac39b9d84eb420d16
itsleejames <176151266+itsleejames@users.noreply.github.com>
2025-11-20 00:54:27 +0000

    New Header

--- Frontend/frontend/src/components/layout/Navbar.jsx
+++ Frontend/frontend/src/components/layout/Navbar.jsx
@@ -2,18 +2,28 @@ import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { FaSearch, FaUser, FaShoppingBag } from "react-icons/fa";
 
 export default function MainNavbar() {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar expand="lg" fixed="top" className="zelora-navbar bg-white shadow-sm py-3">
             <Container>
-                <Navbar.Brand as={Link} to="/">Zelora</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/">ZELORA</Navbar.Brand>
+
                 <Navbar.Toggle aria-controls="main-nav" />
                 <Navbar.Collapse id="main-nav">
-                    <Nav className="ms-auto">
+                    <Nav className="ms-auto align-items-center">
                         <Nav.Link as={Link} to="/">Home</Nav.Link>
                         <Nav.Link as={Link} to="/search">Search</Nav.Link>
                         <Nav.Link as={Link} to="/categories">Categories</Nav.Link>
+
+                        <Nav.Link as={Link} to="/profile" className="ms-3">
+                            <FaUser size={18} />
+                        </Nav.Link>
+
+                        <Nav.Link as={Link} to="/cart" className="ms-2">
+                            <FaShoppingBag size={18} />
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
