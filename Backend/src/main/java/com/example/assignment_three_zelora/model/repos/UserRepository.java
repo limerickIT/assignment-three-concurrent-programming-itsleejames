2c98f56486d54531110dd2980ba84c6f03bf96f0
itsleejames <176151266+itsleejames@users.noreply.github.com>
2025-11-27 00:33:44 +0000

    User Repo

---
+++ Backend/src/main/java/com/example/assignment_three_zelora/model/repos/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.assignment_three_zelora.model.repos;
+
+import com.example.assignment_three_zelora.model.entitys.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
